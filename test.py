1fa0236eb7918b4ff559167c5730c91479d492f2
benjaminymlee <88361634+benjaminymlee@users.noreply.github.com>
2021-09-27 23:12:41 -0700

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from botocore.vendored import requests
-from botocore.exceptions import ClientError
-import boto3
-
-headers = {'host': 'd138jiqnlw2og4.cloudfront.net'}
-response = requests.head('http://d138jiqnlw2og4.ATL50.cloudfront.net/test.txt', headers=headers)
-print(response)
